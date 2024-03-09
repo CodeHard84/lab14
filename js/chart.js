f8df10b25af592bf218e25874c26090f26b1696c
Jace <sockgnome@att.net>
2024-03-09 07:28:08 -0600

    Finished lab 14.

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,55 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
+let state = new AppState();
+state.loadItems();
+
 function renderChart() {
+
+  const labels = [];
+  const viewsData = [];
+  const clicksData = [];
+
+  for (let i = 0; i < state.allProducts.length; i++) {
+    labels.push(state.allProducts[i].name);
+    viewsData.push(state.allProducts[i].timesShown);
+    clicksData.push(state.allProducts[i].timesClicked);
+  }
+
+  // Render chart using Chart.js
+  const canvasChart = document.getElementById('chart').getContext('2d');
+  const myChart = new Chart(canvasChart, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Views',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+          data: viewsData
+        },
+        {
+          label: 'Clicks',
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+          data: clicksData
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
 
 renderChart();
